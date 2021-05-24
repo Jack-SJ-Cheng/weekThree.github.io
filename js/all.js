7daed35e6857941026c82f4560bb6cabf98da2d7
Jack Cheng <randomno@hotmail.com>
2021-05-25 00:05:38 +0800

    20210525 windows

--- js/all.js
+++ js/all.js
@@ -1,5 +1,5 @@
 const app = {
-    data(){
+    data() {
         return {
             url: 'https://vue3-course-api.hexschool.io',
             email: '',
@@ -9,30 +9,45 @@ const app = {
             expired: ''
         }
     },
-    methods:{
-        logIn(){
+    methods: {
+        logIn() {
             const user = {
                 username: this.email,
                 password: this.password
             }
-            axios.post(`${this.url}/admin/signin`,user)
-            .then(res=>{
-                if(res.data.success == true){
-                    console.log(res);
-                    this.token = res.data.token;
-                    this.expired = new Date(res.data.expired);
-                    document.cookie = `hexCookie=${this.token}; expires=${this.expired}`;
-                    location.assign('product.html');
-                }
-            })
-            .catch(err=>console.dir(err))
+            axios.post(`${this.url}/admin/signin`, user)
+                .then(res => {
+                    if (res.data.success == true) {
+                        console.log(res);
+                        this.token = res.data.token;
+                        this.expired = new Date(res.data.expired);
+                        document.cookie = `hexCookie=${this.token}; expires=${this.expired}`;
+                        location.assign('product.html');
+                    }
+                    else {
+                        console.log("登入失敗，請重試。");
+                        alert("帳號或密碼錯誤，請重試。");
+                        this.email = "";
+                        this.password = "";
+                    }
+                })
+                .catch(err => console.dir(err))
         }
     },
-    created(){
+    created() {
+        const cookie = document.cookie.replace(/(?:(?:^|.*;\s*)hexCookie\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+        axios.defaults.headers.common['Authorization'] = cookie;
         axios.post(`${this.url}/api/user/check`)
-        .then(res=>{
-            console.log(res);
-        })
+            .then(res => {
+                if (res.data.success == true) {
+                    console.log("驗證成功，轉入至產品頁", res);
+                    // location.assign('product.html');
+                }
+                else {
+                    console.log("驗證失敗，請重新登入", res);
+                }
+            })
+            .catch(err => console.log(err))
     }
 }
 Vue.createApp(app).mount('#app');
---
+++ js/product.js
@@ -0,0 +1,20 @@
+const app = {
+    data(){
+        return {
+            url: "https://vue3-course-api.hexschool.io",
+            cookie: "",
+            products: []
+        }
+    },
+    methods:{
+        getData(){
+
+        },
+        verify(){
+            
+        }
+    },
+    created(){}
+}
+
+Vue.createApp(app).mount('#app');
