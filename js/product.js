bebf484fe758e09918ac0faba5a1754362441cda
Jack Cheng <randomno@hotmail.com>
2021-05-25 23:36:33 +0800

    finish the delete function

--- js/product.js
+++ js/product.js
@@ -10,13 +10,13 @@ const app = {
         getData(page = 1) {
             axios.get(`${this.url}/api/${this.apiPath}/admin/products?page=${page}`)
                 .then(res => {
-                    if (res.data.success == true) {
+                    if (res.data.success) {
                         const { products } = res.data;
                         this.products = [];
                         products.forEach(item => {
                             this.products.push(item);
                         })
-                    }else if(res.data.success == false){
+                    } else {
                         alert(res.data.message);
                     }
                 })
@@ -25,13 +25,25 @@ const app = {
         verify() {
             axios.post(`${this.url}/api/user/check`)
                 .then(res => {
-                    if (res.data.success == true) {
+                    if (res.data.success) {
                         console.log("驗證成功", res);
                     }
                     else {
                         alert("登入時效已過期，請重新登入");
                         console.log(res);
                         location.assign('index.html');
+                    }
+                })
+                .catch(err => console.log(err))
+        },
+        deleteOne(id, e) {
+            axios.delete(`${this.url}/api/${this.apiPath}/admin/product/${id}`)
+                .then(res => {
+                    if (res.data.success) {
+                        console.log(res);
+                        this.getData();
+                    } else {
+                        alert(res.data.message)
                     }
                 })
                 .catch(err => console.log(err))
